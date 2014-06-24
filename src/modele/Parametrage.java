39427c81e78f7d047f5a320bc05edbcb3cc65789
Adrien <adrien.sy@gmail.com>
2014-06-24 20:51:57 +0200

    Enregistrement des paramètres

---
+++ src/modele/Parametrage.java
@@ -0,0 +1,64 @@
+package modele;
+
+public class Parametrage {
+	public static int TAILLE_DEFAUT = 500;
+	public static int NOMBRE_FOURMIS = 25;
+	public static int QTE_OBSTACLES = 13;
+	public static int QTE_NOURRITURE = 3;
+	public static int VITESSE_EVAPO_PHERO = 25;
+	public static String EMPLACEMENT_SAUVEGARDE = "params_user.txt";
+	public static boolean OBSTACLES_ENTRE_FOURMILIERE_ET_NOURRITURE = false;
+	
+	private int taille, nombreFourmis, qteObstacles, qteFood, vitesseEvapoPhero;
+	private boolean obstacleEntreFourmiliereEtNourriture;
+
+	public Parametrage(){}
+	public Parametrage(int taille, int nbreFourmis, int qteObstacles, int qteFood, int vitesseEvapoPhero, boolean obstacleEntreFourmiliereEtNourriture){
+		this.taille = taille;
+		this.nombreFourmis = nbreFourmis;
+		this.qteObstacles = qteObstacles;
+		this.qteFood = qteFood;
+		this.vitesseEvapoPhero = vitesseEvapoPhero;
+		this.obstacleEntreFourmiliereEtNourriture = obstacleEntreFourmiliereEtNourriture;
+	}
+	
+	public boolean isObstacleEntreFourmiliereEtNourriture() {
+		return obstacleEntreFourmiliereEtNourriture;
+	}
+	
+	public void setObstacleEntreFourmiliereEtNourriture(boolean obstacleEntreFourmiliereEtNourriture) {
+		this.obstacleEntreFourmiliereEtNourriture = obstacleEntreFourmiliereEtNourriture;
+	}
+	public int getTaille() {
+		return this.taille;
+	}
+	public int getNombreFourmis() {
+		  return this.nombreFourmis;
+	}
+	public int getQteObstacles() {
+		return qteObstacles;
+	}
+	public void setQteObstacles(int qteObstacles) {
+		this.qteObstacles = qteObstacles;
+	}
+	public int getQteFood() {
+		return qteFood;
+	}
+	public void setQteFood(int qteFood) {
+		this.qteFood = qteFood;
+	}
+	
+	public int getVitesseEvapoPhero() {
+		return vitesseEvapoPhero;
+	}
+	public void setVitesseEvapoPhero(int vitesseEvapoPhero) {
+		this.vitesseEvapoPhero = vitesseEvapoPhero;
+	}
+	public void setTaille(int taille) {
+		this.taille = taille;
+	}
+	public void setNombreFourmis(int nombreFourmis) {
+		this.nombreFourmis = nombreFourmis;
+	}
+  
+}
--- src/run.java
+++ src/run.java
@@ -6,6 +6,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 
 import vue.MonJFrame;
 import modele.Fichier;
@@ -29,27 +30,46 @@ public class run {
 		String qteObstaclesText = "";
 		String qteNourritureText = "";
 		String vitesseEvapoText = "";
+		String obstacleEntreFoumEtNourText = "";
 		boolean obstacleEntreFoumEtNour = Parametrage.OBSTACLES_ENTRE_FOURMILIERE_ET_NOURRITURE;
 		Parametrage params = null;
 		JFrame parametrageSimulationFrame = new JFrame();
 		int option = 0;
 		
 		JTextField tailleField = new JTextField();
-		tailleField.setText(String.valueOf(Parametrage.TAILLE_DEFAUT));
-		
 		JTextField nombreFourmisField = new JTextField();
-		nombreFourmisField.setText(String.valueOf(Parametrage.NOMBRE_FOURMIS));
-		
 		JTextField qteObstaclesField = new JTextField();
-		qteObstaclesField.setText(String.valueOf(Parametrage.QTE_OBSTACLES));
-		
 		JTextField qteNourritureField = new JTextField();
-		qteNourritureField.setText(String.valueOf(Parametrage.QTE_NOURRITURE));
-		
 		JTextField vitesseEvapoField = new JTextField();
-		vitesseEvapoField.setText(String.valueOf(Parametrage.VITESSE_EVAPO_PHERO));
-		
 		JCheckBox obstacleEntreFoumEtNourBox = new JCheckBox();
+		
+		tailleField.setText(String.valueOf(Parametrage.TAILLE_DEFAUT));
+		nombreFourmisField.setText(String.valueOf(Parametrage.NOMBRE_FOURMIS));
+		qteObstaclesField.setText(String.valueOf(Parametrage.QTE_OBSTACLES));
+		qteNourritureField.setText(String.valueOf(Parametrage.QTE_NOURRITURE));
+		vitesseEvapoField.setText(String.valueOf(Parametrage.VITESSE_EVAPO_PHERO));
+
+		
+		// Si une sauvegarde existe
+		String sauvegarde = Fichier.lire(Parametrage.EMPLACEMENT_SAUVEGARDE);
+		
+		if (!sauvegarde.isEmpty()) {
+			int response1 = JOptionPane.showConfirmDialog(null, "Voulez-vous charger vos derniers paramètres enregistrés ?");
+			
+			if (response1 == JOptionPane.YES_OPTION) {
+				String[] split = sauvegarde.split(System.getProperty("line.separator"));
+				
+				tailleField.setText(String.valueOf(split[0]));
+				nombreFourmisField.setText(String.valueOf(split[1]));
+				qteObstaclesField.setText(String.valueOf(split[2]));
+				qteNourritureField.setText(String.valueOf(split[3]));
+				vitesseEvapoField.setText(String.valueOf(split[4]));
+				
+			}
+		}
+		
+		// Paramétrage de la simulation
+		parametrageSimulationFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		while (option != JOptionPane.CANCEL_OPTION && option != JOptionPane.CLOSED_OPTION &&
 				(tailleText.isEmpty() || nombreFourmisText.isEmpty() || qteObstaclesText.isEmpty() || qteNourritureText.isEmpty() || vitesseEvapoText.isEmpty())) {
@@ -71,13 +91,29 @@ public class run {
 			    qteObstaclesText = qteObstaclesField.getText();
 			    qteNourritureText = qteNourritureField.getText();
 			    vitesseEvapoText = vitesseEvapoField.getText();
-			    if (obstacleEntreFoumEtNourBox.isSelected()) obstacleEntreFoumEtNour = true;
+			    if (obstacleEntreFoumEtNourBox.isSelected()) {
+			    	obstacleEntreFoumEtNour = true;
+			    	obstacleEntreFoumEtNourText = "1";
+			    } else {
+			    	obstacleEntreFoumEtNourText = "0";
+			    }
 			}
 		}
 		
-		Fichier.ecrire(Parametrage.EMPLACEMENT_SAUVEGARDE, "fuck you\r\nNIQUE TA MERE");
-		
 		if (option != JOptionPane.CANCEL_OPTION && option != JOptionPane.CLOSED_OPTION) {
+			int response2 = JOptionPane.showConfirmDialog(null, "Voulez-vous enregistrer vos paramètres ?");
+			
+			if (response2 == JOptionPane.YES_OPTION) {
+				Fichier.ecrire(Parametrage.EMPLACEMENT_SAUVEGARDE,
+								tailleText+System.getProperty("line.separator" )
+								+nombreFourmisText+System.getProperty("line.separator")
+								+qteObstaclesText+System.getProperty("line.separator" )
+								+qteNourritureText+System.getProperty("line.separator" )
+								+vitesseEvapoText+System.getProperty("line.separator" )
+								+obstacleEntreFoumEtNourText
+							);
+			}
+			
 			params = new Parametrage(Integer.valueOf(tailleText), Integer.valueOf(nombreFourmisText), Integer.valueOf(qteObstaclesText), Integer.valueOf(qteNourritureText), Integer.valueOf(vitesseEvapoText), obstacleEntreFoumEtNour);
 			
 			simulation sim = new simulation(params);
